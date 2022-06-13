51d98dd25ec55b70e4a33b28181c7bbfb394b2ad
alejuran <alejandrourn@gmail>
2022-06-12 21:57:03 -0500

    update commit

--- level_0/0-main.py
+++ level_0/0-main.py
@@ -4,7 +4,14 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url="http://158.69.76.135/level0.php"
-
-resp=requests.get(url)
-print(resp)
+url = "http://158.69.76.135/level0.php"
+resp = requests.get(url)
+soup = BeautifulSoup(resp.text, "html.parser")
+inputs = soup.find_all("input")
+data = {
+        "id": "4530",
+        "holdthedoor": "submit"
+}
+bot = requests.post(url, data=data)
+soup = BeautifulSoup(bot.text, "html.parser")
+print(bot.status_code)
